949b3040415fbe1acbc2c8e0a4d181e223988b14
Guilherme Oliveira Casagrande <guiolicasa@gmail.com>
2020-10-28 13:50:07 -0300

    Landing and SelecCharacter pages created and stylized

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -1,14 +1,18 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
+import "../styles/pages/landing.css"
+
 export default function Landing() {
     return (
         <div>
-            <h1>Landing</h1>
-            <Link to="/character">CharacterDetails </Link>
-            <Link to="/create">CreateCharacter </Link>
-            <Link to="/edit">EditCharacter </Link>
-            <Link to="/select">SelectCharacter </Link>
+            <h1 className="title">RPG CHARACTER MANAGER</h1>
+            <div className="link-wrapper">
+                <Link to="/create" className="link">Create a new character </Link>
+            </div>
+            <div className="link-wrapper">
+                <Link to="/select" className="link">Select a created character </Link>
+            </div>
         </div>
     )
 }
