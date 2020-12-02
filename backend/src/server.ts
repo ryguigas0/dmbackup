625ce9b72a01ee560fef62d16468ca0765b73765
Guilherme Oliveira Casagrande <gui6479@gmail.com>
2020-12-02 07:05:04 -0300

    Updated package.json and removed comments

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,38 +10,28 @@ import db from "./config/db"
 
 db()
 const app = express()
-app
-    /* Middlewares */
-    .use(json())
+app.use(json())
     .use(urlencoded({ extended: true }))
     .use(cors())
     .use(morgan("tiny"))
-    /* Error handler */
     .use((err: ErrorRequestHandler, req: Request, res: Response, next: NextFunction) => {
         console.error(err)
         res.status(500).json({
             result: 0
         })
     })
-
-    /* Access to character images */
     .use("/images", express.static(path.join(__dirname, "..", "uploads")))
-    /* Access data */
     .get("/characters", routes.getAllCharacters)
     .get("/character/:id", routes.getCharacter)
-    /* Add data */
     .post("/character", multer(multerConfig as multer.Options).single("avatar"), routes.addCharacter)
     .post("/character/:id/atributes", routes.addCharacterAtribute)
     .post("/character/:id/inventory", routes.addCharacterItem)
-    /* Delete data */
     .delete("/character/:id", routes.deleteCharacter)
     .delete("/character/:characterId/inventory/:itemId", routes.deleteCharacterItem)
     .delete("/character/:characterId/atributes/:atrId", routes.deleteCharacterAtribute)
-    /* Update data */
     .patch("/character/:id", multer(multerConfig as multer.Options).single("avatar"), routes.updateCharacterInfo)
     .patch("/character/:characterId/inventory/:itemId", routes.updateChracterItem)
     .patch("/character/:characterId/atributes/:atrId", routes.updateChracterAtribute)
-
     .listen(`${process.env.PORT}`, () => {
         console.log("Rodando no port: " + `${process.env.PORT}`)
     })
